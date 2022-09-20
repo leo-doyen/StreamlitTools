618b1c4f22e148841bca88bfb0d74a372086c7fb
Nirator78 <51956876+Nirator78@users.noreply.github.com>
2022-09-20 12:11:24 +0200

    suppression import inutile

--- mainPage.py
+++ mainPage.py
@@ -1,8 +1,3 @@
 import streamlit as st
-import os
-import pandas as pd
-from matplotlib import pyplot as plt
-import time
-import numpy as np
 
 st.markdown("# Bienvenue sur la page principale")
--- pages/csvUploader.py
+++ pages/csvUploader.py
@@ -1,9 +1,5 @@
 import streamlit as st
-import os
 import pandas as pd
-from matplotlib import pyplot as plt
-import time
-import numpy as np
 
 
 st.markdown("# CSV Uploader")
