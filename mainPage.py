b65cdfaf7d91554b7e3a980ba28624ac08ab82aa
leo-doyen <leodoyen78@gmail.com>
2022-09-20 11:16:54 +0200

    feat : first commit

---
+++ mainPage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+import time
+import numpy as np
+
+st.markdown("# Bienvenue sur la page principale")
---
+++ pages/tools1.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import os
+import pandas as pd
+from matplotlib import pyplot as plt
+import time
+import numpy as np
+
+
+st.markdown("# Tool 1")
