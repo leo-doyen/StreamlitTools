4656a562309730e6377e4ffb548efad2e8aaa6de
leo-doyen <leodoyen78@gmail.com>
2022-09-20 12:09:40 +0200

    fix : fatal on upload caus of writting byte data

--- pages/csvUploader.py
+++ pages/csvUploader.py
@@ -8,21 +8,29 @@ import numpy as np
 
 st.markdown("# CSV Uploader")
 
+
+option = st.selectbox(
+'Choose separator',
+('/', ',', '#',';',':','\t'))
+st.write('Separator selected:', option)
+
 uploaded_file = st.file_uploader("Choose a CSV file")
+
+
+
+
+
 if uploaded_file is not None:
     # To read file as bytes:
     bytes_data = uploaded_file.getvalue()
-    st.write(bytes_data)
-
-    # To convert to a string based IO:
-    stringio = StringIO(uploaded_file.getvalue().decode("utf-8"))
-    st.write(stringio)
-
-    # To read file as string:
-    string_data = stringio.read()
-    st.write(string_data)
+    # st.write(bytes_data)
 
     # Can be used wherever a "file-like" object is accepted:
-    dataframe = pd.read_csv(uploaded_file)
-    st.write(dataframe)
+    dataframe = pd.read_csv(uploaded_file, sep=option,  encoding='Latin-1')
+    # st.write(dataframe)
+    
+if st.button('Say hello'):
+    dataframe
+else:
+    st.write('Goodbye')
 
